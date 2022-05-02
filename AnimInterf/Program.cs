cb64db00f21294b5a9f17834631eb0b2dbd8c383
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-05-02 18:55:29 +0100

    Create array with random animals, print their sound and type

--- AnimInterf/Program.cs
+++ AnimInterf/Program.cs
@@ -6,7 +6,41 @@ namespace AnimInterf
     {
         static void Main(string[] args)
         {
-            
+            Random rand = new Random();
+
+            Animal[] animals = new Animal[10];
+
+            for (int i = 0; i < animals.Length; i++)
+            {
+                int index = rand.Next(0, 4);
+                
+                switch (index)
+                {
+                    case 0:
+                        animals[i] = new Bat();
+                        break;
+                    case 1:
+                        animals[i] = new Bee();
+                        break;
+                    case 2:
+                        animals[i] = new Cat();
+                        break;
+                    case 3:
+                        animals[i] = new Dog();
+                        break;
+                }
+
+                Console.WriteLine(
+                    $"Animal {i + 1} {animals[i].Sound()}");
+
+                if (animals[i] is IMammal)
+                    Console.WriteLine(
+                        $"- has nipples");
+                
+                if (animals[i] is ICanFly)
+                    Console.WriteLine(
+                        $"- has wings");
+            }
         }
     }
 }
