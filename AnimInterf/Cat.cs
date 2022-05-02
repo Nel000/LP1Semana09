dd6c2a0fbe0261e3f1bfd7c351f685d4140c188c
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-05-02 17:09:08 +0100

    Add Animal classes from previous class

---
+++ AnimInterf/Cat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnimInterf
+{
+    public class Cat : Animal
+    {
+        public override string Sound()
+        {
+            return base.Sound() + "Miau";
+        }
+    }
+}
