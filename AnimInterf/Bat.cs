39d9386fc422670fbe71103f2796316b2753e3ec
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-05-02 18:33:58 +0100

    Implement NumberOfNipples property on mammal classes

--- AnimInterf/Bat.cs
+++ AnimInterf/Bat.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 
 namespace AnimInterf
 {
-    public class Bat : Animal
+    public class Bat : Animal, IMammal
     {
+        public int NumberOfNipples { get => 2; }
+
         public override string Sound()
         {
             return base.Sound() + "Screech!";
