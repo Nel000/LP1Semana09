725ae9e5a0009669ee77cf5dc160ddfeedcb6803
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-05-03 16:33:40 +0100

    Allow user to insert new player into list

--- PlayerManager1/Program.cs
+++ PlayerManager1/Program.cs
@@ -10,6 +10,8 @@ namespace PlayerManager1
             bool hasEnded = false;
 
             string option = "";
+            string currentPlayerName = "";
+            int currentPlayerScore;
 
             List<Player> players = new List<Player>() 
             {
@@ -24,20 +26,33 @@ namespace PlayerManager1
                 Console.WriteLine("-(I)nsert new player");
                 Console.WriteLine("-(L)ist all players");
                 Console.WriteLine(
-                    "-(S)how player with score greater than given value");
+                    "-(S)how players with score greater than given value");
+                Console.WriteLine("-(E)nd application");
 
                 option = Console.ReadLine();
 
                 switch (option)
                 {
                     case "I":
+                        Console.Write("Insert new player name: ");
+                        currentPlayerName = Console.ReadLine();
 
+                        Console.Write("Insert new player score: ");
+                        currentPlayerScore = Convert.ToInt32(Console.ReadLine());
+
+                        players.Add(
+                            new Player(currentPlayerName, currentPlayerScore));
+
+                        Console.WriteLine();
                         break;
                     case "L":
 
                         break;
                     case "S":
 
+                        break;
+                    case "E":
+                        hasEnded = true;
                         break;
                     default:
                         Console.WriteLine("ERROR: Not a valid option");
